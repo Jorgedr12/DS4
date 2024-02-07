86cb6115af6e26569da9dd1dccd7b9fd2fd7e274
Jorgedr12 <jorgedrluisj4@gmail.com>
2024-02-07 14:47:05 -0700

    Cambios

--- Clases/main.py
+++ Clases/main.py
@@ -4,7 +4,7 @@ from Team import Team
 from Game import Game
 import json
 
-def main():
+def old_main():
     players = ['Chicharito','Piojo','Alexis Vega',
                'La Tota','Chuki','Tecatito',
                'Cuahutemoc','Hermoso','Piqué',
@@ -18,7 +18,7 @@ def main():
     t.display()
     players_b = ['Iniesta','Xavi','Neymar','Hulk','Lewandoski','Puyol',
                  'Miguel','Aarón','Francisco','Maradona','Kaká']
-    players_objects2 = [Athlete(x) for x in players]
+    players_objects2 = [Athlete(x) for x in players_b]
     s2 = Sport("Soccer",11,"FIFA")
     t2 = Team("Equipo Ensueño",s2)
     for a in players_objects2:
@@ -30,8 +30,16 @@ def main():
     with open("torneo_soccer.json","w", encoding="utf8") as archivo:
         archivo.write(str(torneo))
 
+def main():
+    with open("torneo_soccer.json","r", encoding="utf8") as archivo:
+        json_leido = json.load(archivo)
+    procesa_diccionario(json_leido)
+
+def procesa_diccionario(diccionario:dict):
+    for k1,v1 in diccionario.items():
+        print(f"key:{k1}")
+        for k2,v2 in v1.items():
+            print(f"key:{k2}")
+
 if __name__ == "__main__":
     main()
-
-
-
---
+++ Practicas/Ruleta.py
@@ -0,0 +1,20 @@
+# Hacer que el usuario pueda ingresar varios datos, que se guarden en una lista y que se haga un sorteo aleatorio de los datos ingresados.
+import random
+
+def datos():
+    Lista = []
+    while True:
+        dato = input("Ingrese un dato: ")
+        if dato == "":
+            break
+        else:
+            Lista.append(dato)
+            
+            
+def sorteo(Lista):
+    print(random.choice(Lista))
+
+
+if __name__ == "__main__":
+    datos = datos()
+    sorteo(datos)
